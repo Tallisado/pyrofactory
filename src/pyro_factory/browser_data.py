3b2dec227172d8ddb52774792b50ac30e4991296
Tallis Vanek <Tallis.Vanek@adtran.com>
2014-04-01 11:47:49 -0400

    prototype

--- src/pyro_factory/browser_data.py
+++ src/pyro_factory/browser_data.py
@@ -12,10 +12,6 @@ class BrowserMode(Enum):
 class BrowserData:
     def __init__(self, config):
         self.url_list = []
-        self.browser_holder = []
-        self.teamcity_intersect = False
-        self.override_username = False
-        self.browser_override = config.DEFAULT_SOLO_BROWSER
         
         # command line overrides
         use_cmdline_username = use_cmdline_accesskey = use_cmdline_display = use_cmdline_local_solo = use_cmdline_browser_sauce = use_cmdline_base_url = False
@@ -39,10 +35,10 @@ class BrowserData:
                 print '(BrowserData) BROWSER found in commandline - will supercede config for local solo'
                 use_cmdline_local_solo = True
             else:
-                print '(BrowserData) ERROR BROWSER found in commandline did not match requirements for sauce or local solo'
+                raise NameError('(BrowserData) ERROR BROWSER found in commandline did not match requirements for sauce or local solo') 
                 # show usage
                 # os=Linux&browser=chrome&browser-version=32
-                exit(1)
+
         
         # determine browser mode
         if os.environ.get('SAUCE_ONDEMAND_BROWSERS') :
@@ -80,45 +76,56 @@ class BrowserData:
                 # os=Linux&browser=chrome&browser-version=32
                 browser_item['os'],browser_item['browser'],browser_item['browser-version'] = browser_mode.split(',')
                 
-                
-               use_cmdline_username = use_cmdline_accesskey = use_cmdline_display = use_cmdline_local_solo = use_cmdline_browser_sauce = use_cmdline_base_url = False
-    def setPyrobotEnvForTest(self, i, config, test_name):
+    def setRuntimeENV(self, i, config, test_name):
         os.environ['BASE_URL'] = config.BASE_URL if not use_cmdline_base_url   
-        os.environ["NO_SAUCE"] = "1" if use_cmdline_local_solo:
+        os.environ["PYBROWSER"] = self.getBrowser(i) if use_cmdline_local_solo:
         os.environ["PYROBOT_REMOTE_URL"] = 'http://%s:%s@ondemand.saucelabs.com:80/wd/hub' % (self.getUserName(i), self.getAccessKey(i))
         os.environ["PYROBOT_CAPS"] = 'name:%s,platform:%s,version:%s,browserName:%s,javascriptEnabled:True' % (test_name, self.getOS(i), self.getBrowserVersion(i), self.getBrowser(i))
+        os.environ['DISPLAY'] = config.DEFAULT_BROWSER_DISPLAY if not use_cmdline_display
         
         print '(BrowserData)[setPyrobotEnvForTest] BASE_URL:            %s' %  os.environ['BASE_URL']
         print '(BrowserData)[setPyrobotEnvForTest] PYROBOT_REMOTE_URL:  %s' %  os.environ['PYROBOT_REMOTE_URL']
         print '(BrowserData)[setPyrobotEnvForTest] PYROBOT_CAPS:        %s' %  os.environ['PYROBOT_CAPS']
-        
-    
+        print '(BrowserData)[setPyrobotEnvForTest] DISPLAY     :        %s' %  os.environ['DISPLAY']
+            
     def getUrlCount(self):
         return len(self.url_list)
         
     def getUrlString(self, index):
         return self.url_list[index]
     
-    def getParsedUrl(self, index):
+    def getUrlHash(self, index):
         self.fields = {}
-        fields = self.getUrlString(index).split(':')[1][1:].split('&')
+        fields = self.url_list[index].split(':')[1][1:].split('&')
         for field in fields:
             [key, value] = field.split('=')   
             self.fields[key] = value
         return self.fields
         
     def getValue(self, target_key, index):   
-        fields = self.getParsedUrl(index)
+        fields = self.getUrlHash(index)
         if target_key in fields:
             return fields[target_key]
         else:
-            return ""
+            return None
 
     def getBrowser(self, index):
-        if self.teamcity_intersect:
-            return self.getValue("browser", index)
+        if browser_mode == BrowserMode.LocalSolo:
+            return 'localbrowser'
         else:
-            return 'local'
+            return self.getValue("browser", index)
+            
+    def getOS(self, index):
+        if browser_mode == BrowserMode.LocalSolo:
+            return platform.system() 
+        else:
+            return self.getValue("os", index)
+
+    def getBrowserVersion(self, index):        
+        if browser_mode == BrowserMode.LocalSolo:
+            return 'localver'
+        else:
+            return self.getValue('browser-version', index)
             
     def getUserName(self, index):
         return self.getValue("username", index)
@@ -129,18 +136,7 @@ class BrowserData:
     def getJobName(self, index):
         return self.getValue("job-name", index)
 
-    def getOS(self, index):
-        if self.teamcity_intersect:
-            return self.getValue("os", index)
-        else:
-            return platform.system()        
 
-    def getBrowserVersion(self, index):        
-        if self.teamcity_intersect:
-            return self.getValue('browser-version', index)
-        else:
-            return 'any' 
-            
     def getFirefoxProfileURL(self, index):
         return self.getValue('firefox-profile-url', index)
 
